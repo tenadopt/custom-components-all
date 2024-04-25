d3d2e5ff4d03b027e5bc12b7047c0e40ab5a0a72
Tenadopt <yury.ignatiev@gmail.com>
2024-04-25 11:16:46 +0300

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,47 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+
+type Mode = 'development' | 'production';
+
+interface EnvVariables {
+    mode: Mode;
+    port: number;
+}
+
+export default (env: EnvVariables) => {
+    const isDev = env.mode === 'development'
+
+    const config: webpack.Configuration = {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: "[name].[contenthash].js",
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
+            new webpack.ProgressPlugin()
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        devtool: isDev && 'inline-source-map',
+        devServer: isDev ? {
+            port: env.port ?? 3000,
+            open: true
+        } : undefined
+    }
+    return config
+}
